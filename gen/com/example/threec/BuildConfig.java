3b351680614ec89d0bad7ba6353ed258a106868c
ckl <ckl1202@sjtu.edu.cn>
2015-10-13 08:25:16 +0800

    first version. add 4 buttons and set the OnClick for the first button

---
+++ gen/com/example/threec/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.threec;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/threec/secondActivity.java
@@ -0,0 +1,7 @@
+package com.example.threec;
+
+import android.app.Activity;
+
+public class secondActivity extends Activity {
+
+}
